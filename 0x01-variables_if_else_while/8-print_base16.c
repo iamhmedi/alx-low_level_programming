7e437a5d87fadca69bb04ca8fb832391bc6428af
iamhmedi <iamhmedi@gmail.com>
2023-03-18 23:57:02 +0100

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - the main function
+* more description
+* Return: the return statement
+*/
+
+int main(void)
+{
+	char C;
+  char x;
+
+	for (C = '0'; C <= '9'; C++)
+	{
+		putchar(C);
+	}
+  for (x = 'a'; x = 'f'; x++)
+  {
+    putchar(x);
+  }
+	putchar('\n');
+	return (0);
+}
