73f5f70eb40393bed1376a49378f29890749a3c8
iamhmedi <iamhmedi@gmail.com>
2023-03-18 22:53:38 +0100

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - the main function
+* more description
+* Return: the return statement
+*/
+
+int main(void)
+{
+	char C;
+
+	for (C = 'a'; C <= 'Z'; C++)
+	{
+		putchar(C);
+	}
+	putchar('\n');
+	return (0);
+}
