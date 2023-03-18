181175e1c0d9dd63799b7fd7ea7e0d25c7da1057
iamhmedi <iamhmedi@gmail.com>
2023-03-18 09:49:21 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,15 @@
 */
 int main(void)
 {
-  char ch = '1';
-  for (ch = '1'; ch <= '27'; ch++)
-  {
-    putchar(ch);
+  //char ch = '1';
+  //for (ch = '1'; ch <= '27'; ch++)
+  //{
+    //putchar(ch);
+  //}
+  
+  char C;
+  for(C = 'a' ; C <= 'z' ; C++) {
+    putchar(C);
   }
   return (0);
 }
