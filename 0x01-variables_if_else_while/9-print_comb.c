1b4ee69a5df94049765ac0c2d113d10bf15d96a1
iamhmedi <iamhmedi@gmail.com>
2023-03-19 00:08:09 +0100

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - the main function
+* more description
+* Return: the return statement
+*/
+
+int main(void)
+{
+	char C;
+
+	for (C = '0'; C <= '9'; C++)
+	{
+		putchar(C);
+    putchar(', ');
+	}
+	putchar('\n');
+	return (0);
+}
