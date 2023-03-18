12a0a0afcfa6fd0d6b4e30ca3c54daedd06fdbe2
iamhmedi <iamhmedi@gmail.com>
2023-03-18 22:59:40 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,10 +11,15 @@
 int main(void)
 {
 	char C;
+	char X;
 
 	for (C = 'a'; C <= 'z'; C++)
 	{
 		putchar(C);
+	}
+	for (X = 'A'; X <= 'Z'; X++)
+	{
+		putchar(X);
 	}
 	putchar('\n');
 	return (0);
