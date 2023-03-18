33412f4ef902c19fdc0a5c3bf9ee718cf181bd89
iamhmedi <iamhmedi@gmail.com>
2023-03-18 23:03:31 +0100

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - the main function
+* more description
+* Return: the return statement
+*/
+
+int main(void)
+{
+	char C;
+
+	for (C = 'a'; C <= 'z'; C++)
+	{
+    if (C != 'e' && C != 'q')
+    {
+      putchar(C);
+    }
+	}
+	putchar('\n');
+	return (0);
+}
