ad397707f6ed8bb2090063a73c40554ff271d0be
iamhmedi <iamhmedi@gmail.com>
2023-03-18 09:35:36 +0100

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - the main function
+* more description
+* Return: the return statement
+*/
+int main(void)
+{
+  // Get the character to be written
+  char ch = '1';
+  // Write the Character to stdout
+  for (ch = '1'; ch <= '9'; ch++)
+    putchar(ch);
+  return (0);
+}
