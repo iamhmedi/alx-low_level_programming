38d4a1c0f86e7c7daf81cba7b928b369a520717d
iamhmedi <iamhmedi@gmail.com>
2023-03-19 05:53:41 +0100

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - the main function
+* more description
+* Return: the return statement
+*/
+
+int main(void)
+{
+	int C, X;
+
+	for (C = '0'; C <= '9'; C++)
+	{
+		for (X = C +1; X <= '9'; X++)
+		{
+			if (X != C)
+			{
+				putchar('C');
+				putchar('X');
+				if( C == '8' && X == '9')
+				continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
