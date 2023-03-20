e806ff7c9e5021a25f60c86d6a3925ced1775494
iamhmedi <iamhmedi@gmail.com>
2023-03-19 17:18:18 -0700

    printf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - the main function
+* more description
+* Return: the return statement
+*/
+
+int main(void)
+{
+	int i;
+	char c;
+	long int l;
+	long long int o;
+	float f;
+
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(o));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
