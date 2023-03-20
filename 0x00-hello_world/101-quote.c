1c3d9abce62c2ae761915ca052da809ad51d7cbd
iamhmedi <iamhmedi@gmail.com>
2023-03-19 22:06:10 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - the main function
+* more description
+* Return: the return statement
+*/
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
