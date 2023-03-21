7cd919fd94a3eea33fc927c9ef754512eca479d7
iamhmedi <iamhmedi@gmail.com>
2023-03-21 08:20:15 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - the main function
+* more description
+* Return: the return statement
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
