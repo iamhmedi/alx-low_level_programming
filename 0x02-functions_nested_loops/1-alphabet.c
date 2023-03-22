c0faf4f37b4367ae263d9f29d46fbcbb025dc409
iamhmedi <iamhmedi@gmail.com>
2023-03-22 19:04:32 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,13 +8,13 @@
 */
 
 /**
- * main - Entry point
- * 
- * Description: 'the program's description'
- * @parameter: describe the parameter
- * 
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Description: 'the program's description'
+* @parameter: describe the parameter
+*
+* Return: Always 0 (Success)
+*/
 
 void print_alphabet(void)
 {
