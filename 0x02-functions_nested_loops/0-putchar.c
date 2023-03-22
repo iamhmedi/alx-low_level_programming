6021507574cd79f47e041678b0cbb6498818d1e4
iamhmedi <iamhmedi@gmail.com>
2023-03-21 23:45:59 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,4 @@
-/**
-#include <stdlib.h>
-#include <time.h>
-*/
-#include <stdio.h>
+#include "main.h"
 
 /**
 * main - the main function
@@ -12,6 +8,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 	return (0);
 }
