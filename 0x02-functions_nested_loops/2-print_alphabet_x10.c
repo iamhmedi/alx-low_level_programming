7e0052609cc0289a3607271abdfe31d94e8eb222
iamhmedi <iamhmedi@gmail.com>
2023-03-22 19:24:42 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,12 @@ void print_alphabet(void)
 {
 	char alpha;
 
-  for (int count = 0; count <= 9; count++)
-  {
-	  for (alpha = 'a'; alpha <= 'z'; alpha++)
-	  {
-		  _putchar(alpha);
-	  }
-  }
+	for (int count = 0; count <= 9; count++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+	}
 	_putchar('\n');
 }
