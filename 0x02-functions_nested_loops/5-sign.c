f5aaa193710be8bdffabe9060b0a487914ef2154
iamhmedi <iamhmedi@gmail.com>
2023-03-22 20:32:06 +0100

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_sign - check character case
+* @n: parameter to be checked
+* Return: 1 if is lower cas else 0
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+    return (1);
+	}
+	else if (n == 0)
+  {
+		return (0);
+	}
+}
