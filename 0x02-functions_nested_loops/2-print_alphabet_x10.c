89a466282e6e618454e5339b75417b58b92028bb
iamhmedi <iamhmedi@gmail.com>
2023-03-22 19:22:04 +0100

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_alphabet - print alphabets
+*/
+
+void print_alphabet(void)
+{
+	char alpha;
+
+  for (int count = 0; count <= 9; count++)
+  {
+	  for (alpha = 'a'; alpha <= 'z'; alpha++)
+	  {
+		  _putchar(alpha);
+	  }
+  }
+	_putchar('\n');
+}
