2d96c0fee5e809e063bf6ec3d7a4fee8dcdf322b
iamhmedi <iamhmedi@gmail.com>
2023-03-22 20:02:37 +0100

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isalpha - check character case
+* @c: parameter to be checked
+* Return: 1 if is lower cas else 0
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
