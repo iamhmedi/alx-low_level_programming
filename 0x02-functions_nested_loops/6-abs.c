52fc47aa18e89ba7d43c9a32e2f871b16786c373
iamhmedi <iamhmedi@gmail.com>
2023-03-23 05:45:35 +0100

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _abs - check character case
+* @n: parameter to be checked
+* Return: 1 if is lower cas else 0
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+    int abs_val;
+    
+    abs_val = c * -1;
+		return (abs_val);
+	}
+  return (n);
+}
