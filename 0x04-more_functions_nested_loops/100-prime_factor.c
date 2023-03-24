49792363496fc00a508010e131119c8a20242b64
iamhmedi <iamhmedi@gmail.com>
2023-03-24 05:31:37 +0100

    Create 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - print something
+* Return: returns 0
+*/
+
+int main(void)
+{
+	long x, max;
+	long number = 612852475143;
+	double square = sqrt(number);
+	
+	for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			max = number / x;
+		}
+	}
+	printf("%ld\n", max);
+	return (0);
+}
