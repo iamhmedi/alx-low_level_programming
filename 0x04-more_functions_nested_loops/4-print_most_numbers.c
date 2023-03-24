c29b3491c640c6caa585dff81303ff1444733c36
iamhmedi <iamhmedi@gmail.com>
2023-03-24 03:04:05 +0100

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_most_numbers - ...
+* Return: 0 or 1
+*/
+
+void print_most_numbers(void)
+{
+	int x;
+	
+	for (x = 0; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
+	}
+	_putchar('\n');
+}
