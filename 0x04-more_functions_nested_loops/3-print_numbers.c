a806b2993235ea9c05d0fce291bb1bb6ae97bdf3
iamhmedi <iamhmedi@gmail.com>
2023-03-24 02:53:51 +0100

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_numbers - ...
+* Return: 0 or 1
+*/
+
+void print_numbers(void)
+{
+	int x;
+	
+	for (x = 0; x <= 9; x++)
+		_putchar(x + '0');
+	_putchar('\n');
+}
