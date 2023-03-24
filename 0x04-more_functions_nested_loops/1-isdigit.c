84c68c7e63881048c097be8645efffc56d40523f
iamhmedi <iamhmedi@gmail.com>
2023-03-24 02:32:38 +0100

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _isdigit - uppercase letter
+* @c: char to check
+* Return: 0 or 1
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return(1);
+	return (0);
+}
