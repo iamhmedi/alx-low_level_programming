4b08f781ac5d96795a88dc8fc2b2b45acf26c3a4
iamhmedi <iamhmedi@gmail.com>
2023-03-27 10:11:56 +0100

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_rev - description
+* @s: description
+* Return: void
+*/
+
+void print_rev(char *s)
+{
+	int c = 0;
+	
+	while(s[c] != '\0')
+	{
+		c++;
+	}
+	
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
