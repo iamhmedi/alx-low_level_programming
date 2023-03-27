b9d64f37c7a2aed8135c4ddb80eb35086eb40af2
iamhmedi <iamhmedi@gmail.com>
2023-03-27 10:19:47 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,12 +11,12 @@ void rev_string(char *s)
 {
 	int len = 0, index = 0;
 	char temp;
-	
-	while(s[index++])
+
+	while (s[index++])
 	{
 		len++;
 	}
-	
+
 	for (index = len - 1; index >= len / 2; index--)
 	{
 		temp = s[index];
