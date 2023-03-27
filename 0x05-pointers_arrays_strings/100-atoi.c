0b692fdb6afb49308a456586830b165033be3f4a
iamhmedi <iamhmedi@gmail.com>
2023-03-27 11:13:03 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -20,14 +20,14 @@ int _atoi(char *s)
 		{
 			min *= -1;
 		}
-		
+
 		while (s[c] >= 48 && s[c] <= 57)
 		{
 			isi = 1;
 			ni = (ni * 10) + (s[c] - '0');
 			c++;
 		}
-		
+
 		if (isi == 1)
 		{
 			break;
