8aa2d5fc0d624d556fe1b2d1bd130da402b9b004
iamhmedi <iamhmedi@gmail.com>
2023-03-29 15:49:36 +0100

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* reverse_array - description
+* @a: description
+* @n: description
+*/
+
+char reverse_array(int *a, int n)
+{
+	int tmp, index;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
+}
