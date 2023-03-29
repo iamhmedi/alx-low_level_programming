a08b15bd7c30b986829fdf3d9c25fec77c5a500d
iamhmedi <iamhmedi@gmail.com>
2023-03-29 11:06:08 +0100

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strcat - description
+* @dest: description
+* @src: description
+* Return: pointer
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, j;
+	
+	while (dest[len])
+	{
+		len++;
+	}
+
+	for (j = 0; src[j] != 0; j++)
+	{
+		dest[len] = src[j];
+		len++;
+	}
+	dest[len] = '\0');
+	return (dest);
+}
