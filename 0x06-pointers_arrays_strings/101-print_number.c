1692f7bcfe8874fe0127ab9b8a72398f03c0aa9f
iamhmedi <iamhmedi@gmail.com>
2023-03-30 06:12:07 +0100

    Create 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_number - description
+* @n: description
+*/
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if ((num / 10) > 0)
+	print_number(number / 10);
+	_putchar((num % 10) + '0');
+}
