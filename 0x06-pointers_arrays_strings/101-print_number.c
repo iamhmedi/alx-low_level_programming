c03e7d92f8b9003ecd18522e61856ae58c9b1a78
iamhmedi <iamhmedi@gmail.com>
2023-03-30 15:16:42 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,20 +3,22 @@
 /**
 * print_number - description
 * @n: description
+* Return: 0
 */
 
 void print_number(int n)
 {
 	unsigned int num;
 
+	num = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		num = -n;
 	}
-	if ((num / 10) > 0)
+	if (num / 10 != 0)
 	{
-		print_number(number / 10);
-		_putchar((num % 10) + '0');
+		print_number(num / 10);
 	}
+	_putchar((num % 10) + '0');
 }
