e1499a34b5661a10d0802d304b8db75cf5799e4f
iamhmedi <iamhmedi@gmail.com>
2023-03-30 06:35:55 +0100

    Create 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* rot13 - description
+* @s: description
+* Return: pointer
+*/
+
+char *rot13(char *s)
+{
+	int i, j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	car datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
+	}
+}
